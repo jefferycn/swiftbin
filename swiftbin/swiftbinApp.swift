aeb57ed66eb8ca5dc317d0bd73ad593181c5ef0c
Jeffery You <jyou@adobe.com>
2023-02-25 18:41:23 -0800

    Initial Commit

---
+++ swiftbin/swiftbinApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftbinApp.swift
+//  swiftbin
+//
+//  Created by Jeffery You on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftbinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
